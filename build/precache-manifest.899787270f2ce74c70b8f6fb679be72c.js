6c1e8903a35c261238bf3c0616abac0cbd52214f
ashokmachineni <ashok5679m@gmail.com>
2020-08-19 12:52:20 +0100

    added build

---
+++ build/precache-manifest.899787270f2ce74c70b8f6fb679be72c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "064cc88aed0b01a3488c629d075721d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16ce98794b29c6e9f313",
+    "url": "/static/css/main.1ca44591.chunk.css"
+  },
+  {
+    "revision": "3554a410798153acaf02",
+    "url": "/static/js/2.e88d316f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.e88d316f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16ce98794b29c6e9f313",
+    "url": "/static/js/main.4ac46377.chunk.js"
+  },
+  {
+    "revision": "756133eb3ee7e882e75a",
+    "url": "/static/js/runtime-main.355578c1.js"
+  },
+  {
+    "revision": "80a9df8e4a806e84b04545e7346cb707",
+    "url": "/static/media/FoundersGrotesk-Regular.80a9df8e.woff2"
+  },
+  {
+    "revision": "adafc2c81f6c1a26389d3740568d3ee9",
+    "url": "/static/media/UniviaPro-Regular.adafc2c8.woff"
+  },
+  {
+    "revision": "10c6e873fb6b67f23b502bb124183c53",
+    "url": "/static/media/background.10c6e873.svg"
+  },
+  {
+    "revision": "c04cf9200b6c86c9593e73c9640243ee",
+    "url": "/static/media/movie.c04cf920.png"
+  },
+  {
+    "revision": "0584283a439246b31e48e33cd3e31680",
+    "url": "/static/media/vino-li-gGX1fJkmw3k-unsplash-min.0584283a.jpg"
+  }
+]);
